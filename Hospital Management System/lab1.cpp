1d12ad86faef23a2bd867fe930047a73d87abcaf
Trishita71 <147441875+Trishita71@users.noreply.github.com>
2025-06-28 23:28:10 +0600

    Add files via upload

---
+++ Hospital Management System/lab1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //This is a single line
+    cout<<"Compiler Lab"<<endl;
+    /*This
+    is
+    a 
+    multiple
+    line 
+    */
+   int x = 5;
+   cout<<x;
+   return 0;
+
+}
